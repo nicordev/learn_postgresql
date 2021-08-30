629123ab5d24c676ed2b17711983d643eb78213d
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-08-30 22:29:55 +0200

    improve date

--- date/date_substract.sql
+++ date/date_substract.sql
@@ -1,4 +1,9 @@
-select '2021-08-14 00:00:00'::date - interval '4 days';
-
 -- how to properly cast to date:
-select cast('2021-06-07' as date)
+select cast('2021-06-07' as date);
+
+-- substraction
+select cast('2021-11-07' as date) - cast('2021-08-14' as date);
+
+-- interval
+select cast('2021-11-07' as date) - interval '4 days';
+select '2021-08-14 00:00:00'::date - interval '4 days';
