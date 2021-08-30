3d67586e707025bbfffa1653f4436a35a406d841
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-08-30 16:01:08 +0200

    add json query between 2 dates

--- json/json.sql
+++ json/json.sql
@@ -2,3 +2,6 @@ select jsonb_array_elements(json_table_alias.property_name -> 'child_property_ar
 
 select * from json_table_name json_table_alias, jsonb_array_elements(json_table_alias.json_property_name -> 'child_property_array') property_alias
 where property_alias -> 'otherProperty' ->> 'oneLastProperty' = 'myValue'
+
+select * from json_table_name json_table_alias, jsonb_array_elements(json_table_alias.json_property_name -> 'child_property_array') property_alias
+where cast(property_alias -> 'otherProperty' ->> 'oneLastProperty' as date) between cast('2020-09-11' as date) and cast('2020-09-15' as date)
