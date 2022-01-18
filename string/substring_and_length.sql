b25177c5dcf7b9c856bc6241011c5978c1a6a159
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2022-01-18 12:03:17 +0100

    improve substring

--- string/substring_and_length.sql
+++ string/substring_and_length.sql
@@ -1,4 +1,9 @@
+-- substring(string, start, end)
+select substring('hello world!', 1, 5);
+select substring(now()::text, 1, 10) as today;
 
+-- using starting position and length
+-- substring(string from start for length)
 update fruits
 set fruit_name = substring(fruit_name from 34 for 42)
 where fruit_name like 'myLooongName%'
