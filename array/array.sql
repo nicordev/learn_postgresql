d1ab288e69d3cee47076f9dc23cc1225ab823868
nicolas renvoise <nicolas.renvoise@alximy.io>
2023-08-26 08:12:51 +0200

    add array, json

--- array/array.sql
+++ array/array.sql
@@ -3,3 +3,19 @@
 SELECT ARRAY(
     SELECT ARRAY[i, i*2] FROM generate_series(1,5) AS a(i)
 );
+
+create temporary table fruit(
+    id bigserial primary key,
+    name varchar (255)
+)
+;
+
+insert into fruit(id, name)
+values (123, 'banana')
+;
+
+select array[
+    f.id
+    -- , f.name
+] from fruit f
+;
--- json/json_build_object.sql
+++ json/json_build_object.sql
@@ -1,2 +1,5 @@
 
 SELECT JSON_BUILD_OBJECT('id', 12, 'title', 'hello');
+
+select 
+    json_build_object('promotions', jsonb_build_object('one', 1, 'two', 2), 'items', ARRAY[1, 2]) AS json_test;
