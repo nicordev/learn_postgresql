b122bdf406c9d11a4795b57ff59b3a1949a28e8e
nicolas renvoise <nicolas.renvoise@alximy.io>
2023-08-31 16:48:58 +0200

    improve random

--- random/random.sql
+++ random/random.sql
@@ -1,14 +1,13 @@
 create temporary table fruit(
-    id bigserial primary key,
     name varchar (255)
 )
 ;
 
 insert into fruit(name)
 values
-('apple'),
-('orange'),
-('banana')
+    ('apple'),
+    ('orange'),
+    ('banana')
 ;
 
 select "name"
